29f6223d560ce57f553e2ff286d460b2b417e3c8
Andrei Riaskov <ryaskov.work@gmail.com>
2017-07-16 12:40:11 +0300

    Added class SmartTableRecord. Moved enum Sex to common.dart.

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,6 @@
 import 'package:angular2/platform/browser.dart';
 import 'smart_table_component.dart';
 
-enum Sex {male, female}
 
 void main() {
   bootstrap(SmartTableComponent);
---
+++ web/smart_table_record.dart
@@ -0,0 +1,39 @@
+import 'common.dart';
+import 'Department.dart';
+import 'Address.dart';
+
+class SmartTableRecord {
+  String m_name;
+  int m_age;
+  Sex m_sex;
+  Department m_department;
+  Address m_address;
+
+  String get name => m_name;
+  void set name(String name) {
+    m_name = name;
+  }
+
+  int get age => m_age;
+  void set age(int age) {
+    m_age = age;
+  }
+
+  Sex get sex => m_sex;
+  void set sex(Sex sex) {
+    m_sex = sex;
+  }
+
+  Department get department => m_department;
+  void set department(Department department) {
+    m_department = department;
+  }
+
+  Address get address => m_address;
+  void set address(Address address) {
+    m_address = address;
+  }
+
+
+  SmartTableRecord(this.m_name, this.m_age, this.m_sex, this.m_department, this.m_address);
+}
