eab3325659a0845ab143e57d0647c36dc3a8f97a
Andrei Riaskov <ryaskov.work@gmail.com>
2017-07-17 09:43:53 +0300

    Extracted smart table component's template to separate html-file.

--- web/smart_table_component.dart
+++ web/smart_table_component.dart
@@ -3,19 +3,7 @@ import 'common.dart';
 
 @Component(
   selector: 'smart-table-component',
-  template: '''
-      <table>
-      <th>Имя</th><th>Возраст</th><th>Департамент</th><th>Адрес</th>
-      <tr *ngFor="#record of m_dataService.data">
-      <td>{{record.name}}</td>
-      <td>{{record.age}}</td>
-      <td>{{record.sex}}</td>
-      <td>{{record.department.name}}</td>
-      <td>{{record.address.city}}, {{record.address.street}} {{record.address.building}}</td>
-      </tr>
-      <tr>
-      </table>
-    ''',
+  templateUrl: 'smart_table_component.html',
   providers: const [SmartTableRecordDataService]
 )
 class SmartTableComponent {
