799fbc13a9a9690b069198316f086d4fd432a14e
Andrei Riaskov <ryaskov.work@gmail.com>
2017-07-18 14:15:59 +0300

    Added OrderBy pipe for sorting column values.

---
+++ web/order_by_pipe.dart
@@ -0,0 +1,49 @@
+import 'dart:mirrors';
+import 'package:angular2/core.dart';
+import 'common.dart';
+import 'smart_table_record.dart';
+
+@Pipe('orderBy')
+class OrderByPipe implements PipeTransform {
+  int _convertSortOrderToInt(String order){
+    int result = 0;
+    if (order.compareTo('asc') == 0){
+      result = -1;
+    }else if (order.compareTo('des') == 0){
+      result = 1;
+    }
+    return result;
+  }
+
+  List<SmartTableRecord> transform(List<SmartTableRecord> array, String columnName, String order) {
+    bool isColumnExists = false;
+    Symbol simpleNameAsSymbol;
+    ClassMirror classMirror = reflectClass(SmartTableRecord);
+    for (DeclarationMirror m in classMirror.declarations.values) {
+      simpleNameAsSymbol = m.simpleName;
+      String simpleName = MirrorSystem.getName(m.simpleName);
+      if (columnName.compareTo(simpleName) == 0) {
+        isColumnExists = true;
+        break;
+      }
+    }
+    if (isColumnExists) {
+      array.sort((SmartTableRecord a, SmartTableRecord b) {
+        var value1 = reflect(a).getField(simpleNameAsSymbol).reflectee;
+        var value2 = reflect(b).getField(simpleNameAsSymbol).reflectee;
+        if (value1 is Comparable != true) {
+          value1 = value1.toString();
+          value2 = value2.toString();
+        }
+        if (value1.compareTo(value2) < 0){
+          return 1 * _convertSortOrderToInt(order);
+        } else if (value1.compareTo(value2) > 0) {
+          return -1 * _convertSortOrderToInt(order);
+        } else {
+          return 0;
+        }
+      });
+      return array;
+    }
+  }
+}
--- web/smart_table_component.dart
+++ web/smart_table_component.dart
@@ -1,16 +1,17 @@
 import 'package:angular2/angular2.dart';
 import 'common.dart';
+import 'order_by_pipe.dart';
 
 @Component(
   selector: 'smart-table-component',
   templateUrl: 'smart_table_component.html',
-  providers: const [SmartTableRecordDataService]
+  providers: const [SmartTableRecordDataService],
+  pipes: const [OrderByPipe]
 )
 class SmartTableComponent {
   final SmartTableRecordDataService m_dataService;
-
   SmartTableComponent(this.m_dataService);
-
+  
   String getSexAsString(Sex sex) {
     String result  = 'Male';
     if (sex.index == Sex.female.index){
